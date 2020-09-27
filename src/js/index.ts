6dcc39f9299cbea6910541802a72ea6c09e3a2e6
Katerina Komarkova <kate2135@edu.easj.dk>
2020-09-27 18:59:22 +0200

    changes

--- src/js/index.ts
+++ src/js/index.ts
@@ -32,10 +32,29 @@ console.log(d.lastElementChild.classList.replace("healthy", "unhealthy"))
 
 let node = document.createElement("li")
 let textnode = document.createTextNode("Strawberries")
-node.setAttribute("id","novy")
+node.setAttribute("id","strawberries")
 node.classList.add("healthy")
 node.appendChild(textnode)
 document.getElementById("list").appendChild(node)
+
+let u = document.getElementById("list")
+let n = document.createElement("li")
+let text = document.createTextNode("Pears")
+n.setAttribute("id","pears")
+n.classList.add("healthy")
+n.appendChild(text)
+
+u.insertBefore(n, u.childNodes[0]);
+
+//Move a node
+let newList:HTMLCollectionOf<HTMLLIElement> =
+    <HTMLCollectionOf<HTMLLIElement>> document.getElementsByClassName("unhealthy");
+    
+// Delete element in the list
+var list = document.getElementById("list")
+list.removeChild(list.childNodes[0]);
+
+
 
 
 
